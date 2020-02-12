141092758476a56531d67472ad385caaf0a0dc7c
David <chung.david@live.ca>
2020-02-11 22:03:34 -0800

    Wrote tests for PrintedCircuitBoardTest and SwitchesTest

--- src/test/model/PrintedCircuitBoardTest.java
+++ src/test/model/PrintedCircuitBoardTest.java
@@ -1,4 +1,35 @@
 package model;
 
+import model.components.PrintedCircuitBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PrintedCircuitBoardTest {
+    PrintedCircuitBoard testPCB;
+
+    @BeforeEach
+    void runBefore() {
+        testPCB = new PrintedCircuitBoard();
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("",testPCB.getPcbSize());
+    }
+
+    @Test
+    void testGetPCBSize() {
+        assertEquals("",testPCB.getPcbSize());
+        testPCB.setPcbSize("small");
+        assertEquals("small",testPCB.getPcbSize());
+    }
+
+    @Test
+    void testSetPCBSize() {
+        assertEquals("",testPCB.getPcbSize());
+        testPCB.setPcbSize("medium");
+        assertEquals("medium",testPCB.getPcbSize());
+    }
 }
--- src/test/model/SwitchesTest.java
+++ src/test/model/SwitchesTest.java
@@ -1,4 +1,66 @@
 package model;
 
+import model.components.Switches;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SwitchesTest {
+    Switches testSwitches;
+
+    @BeforeEach
+    void runBefore() {
+        testSwitches = new Switches();
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("",testSwitches.getSwitchType());
+        assertFalse(testSwitches.isSilentSwitches());
+    }
+
+    @Test
+    void testGetSwitchType() {
+        assertEquals("",testSwitches.getSwitchType());
+        testSwitches.setSwitchType("tactile");
+        assertFalse(testSwitches.isSilentSwitches());
+        assertEquals("tactile",testSwitches.getSwitchType());
+    }
+
+    @Test
+    void testIsSilentSwitchesFalse() {
+        assertEquals("",testSwitches.getSwitchType());
+        testSwitches.setSwitchType("linear");
+        assertFalse(testSwitches.isSilentSwitches());
+        assertEquals("linear",testSwitches.getSwitchType());
+    }
+
+    @Test
+    void testIsSilentSwitchesTrue() {
+        assertEquals("",testSwitches.getSwitchType());
+        testSwitches.setSwitchType("linear");
+        assertFalse(testSwitches.isSilentSwitches());
+        testSwitches.setSilentSwitches(true);
+        assertEquals("linear",testSwitches.getSwitchType());
+        assertTrue(testSwitches.isSilentSwitches());
+    }
+
+    @Test
+    void testSetSwitchType() {
+        assertEquals("",testSwitches.getSwitchType());
+        testSwitches.setSwitchType("clicky");
+        assertFalse(testSwitches.isSilentSwitches());
+        assertEquals("clicky",testSwitches.getSwitchType());
+    }
+
+    @Test
+    void testSetSilentSwitches() {
+        assertEquals("",testSwitches.getSwitchType());
+        testSwitches.setSwitchType("linear");
+        assertFalse(testSwitches.isSilentSwitches());
+        testSwitches.setSilentSwitches(true);
+        assertEquals("linear",testSwitches.getSwitchType());
+        assertTrue(testSwitches.isSilentSwitches());
+    }
 }
