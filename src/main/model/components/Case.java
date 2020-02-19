f91a89e1e0d031eb4ed6b1a8eb66d9e1803b9ef8
David <chung.david@live.ca>
2020-02-19 01:07:01 -0800

    ~~Transfer~~ Start Refactorization

--- src/main/model/components/Case.java
+++ src/main/model/components/Case.java
@@ -2,8 +2,8 @@ package model.components;
 
 // Represents a case of a keyboard that indicates what material it is made out of as well as the size of the case.
 public class Case {
-    private String caseMaterial;
-    private String caseSize;
+    private static String caseMaterial;
+    private static String caseSize;
 
     // EFFECTS: Constructs a case with no caseMaterial and no caseSize
     public Case() {
@@ -13,26 +13,25 @@ public class Case {
 
     // Getters
     // EFFECTS: returns the material of the case
-    public String getCaseMaterial() {
+    public static String getCaseMaterial() {
         return caseMaterial;
     }
 
     // EFFECTS: returns the size of the case
-    public String getCaseSize() {
+    public static String getCaseSize() {
         return caseSize;
     }
 
     // Setters
     // MODIFIES: this
     // EFFECTS: sets the material of the case
-    public void setCaseMaterial(String caseMaterial) {
+    public static void setCaseMaterial(String caseMaterial) {
         Case.caseMaterial = caseMaterial;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the size of the case
-    public void setCaseSize(String caseSize) {
+    public static void setCaseSize(String caseSize) {
         Case.caseSize = caseSize;
     }
 }
-Test
