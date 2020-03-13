e5b699a368f76cebe4935c60be5bc9c5b904999a
David <chung.david@live.ca>
2020-03-12 18:31:14 -0700

    Added GUI class in ui package and built a scene

---
+++ src/main/ui/GUI.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+
+        button = new Button();
+        button.setText("Click Me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout,300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
